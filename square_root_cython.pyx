9cbd7c9b46849cf6403977e6d95eb21c4bc53a53
sharmaeishan <135609354+sharmaeishan@users.noreply.github.com>
2024-05-12 10:34:53 +0530

    Update square_root_cython.pyx

--- square_root_cython.pyx
+++ square_root_cython.pyx
@@ -1,10 +1,14 @@
 # square_root_cython.pyx
 
 
-import numpy as np
-cimport numpy as np
-
 cpdef int calculate_square_root_cython(int number):
-    cdef int root = int(np.sqrt(number))
+    cdef int root = 0
+    cdef int i
+    for i in range(number + 1):
+        if i * i == number:
+            root = i
+            break
+        elif i * i > number:
+            break
     return root
 
