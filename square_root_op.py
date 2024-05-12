bdc50b5bdf9a1035812c360e09ed7054723cc2d3
sharmaeishan <135609354+sharmaeishan@users.noreply.github.com>
2024-05-12 10:35:33 +0530

    Update square_root_op.py

--- square_root_op.py
+++ square_root_op.py
@@ -1,11 +1,9 @@
 # square_root_op.py
 
-import numpy as np
-
 def calculate_square_root(numbers):
     if isinstance(numbers, int):
         numbers = [numbers]  # Convert single integer to a list
     roots = []
     for num in numbers:
-        roots.append(np.sqrt(num))
+        roots.append(num**0.5)
     return roots
